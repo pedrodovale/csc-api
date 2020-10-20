3887203d1279fd510272087406168dcebf87b326
pvale <pedro.vale@multicert.com>
2020-10-20 22:07:22 +0100

    Fix info controller autowire bsl service

--- ws/src/main/java/com/pvale/project/csc/ws/config/WsConfig.java
+++ ws/src/main/java/com/pvale/project/csc/ws/config/WsConfig.java
@@ -1,9 +1,12 @@
 package com.pvale.project.csc.ws.config;
 
+import com.pvale.project.csc.bsl.config.BslConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(BslConfig.class)
 @ComponentScan(basePackages = {
         "com.pvale.project.csc.ws.controller"
 })
